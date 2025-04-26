447053e92c18ce6fc4ba2543def22eea4b5a8ff8
Szymon Kostrubiec <github.sk99@gmail.com>
2025-04-26 19:33:38 +0200

    Added user entity, repository and default fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,37 @@ namespace SkPro\Standard\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use SkPro\Standard\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher
+    )
+    {
+        
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $manager->flush();
+    }
+
+    public function createUsers(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('user@example.com');
+        $user->setRoles(['ROLE_USER']);
+        $$user->setPassword($this->passwordHasher->hashPassword($user, 'user'));
+
+        $manager->persist($user);
+
+        $admin = new User();
+        $admin->setEmail('admin@example.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'admin'));
+
+        $manager->persist($admin);
 
         $manager->flush();
     }
